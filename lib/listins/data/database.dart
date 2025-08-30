fdaf68f79258785e42d9f315599616f009eefab8
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2025-08-30 20:13:18 -0300

    configurando a conexao com o banco

--- lib/listins/data/database.dart
+++ lib/listins/data/database.dart
@@ -1,4 +1,9 @@
+import "dart:io";
+
 import "package:drift/drift.dart";
+import "package:drift/native.dart";
+import "package:path_provider/path_provider.dart";
+import 'package:path/path.dart' as path;
 
 part "database.g.dart";
 
@@ -12,8 +17,17 @@ class ListiTable extends Table {
 
 @DriftDatabase(tables: [ListiTable])
 class AppDatabase extends _$AppDatabase {
-  AppDatabase(super.e);
+  AppDatabase() : super(_openConnection());
 
   @override
   int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(path.join(dbFolder.path, "db.sqlite"));
+
+    return NativeDatabase.createInBackground(file);
+  });
 }
