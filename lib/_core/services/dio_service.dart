59724818c83b227e945a69a2b093fbe9ee9b2655
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2025-09-02 16:14:01 -0300

    fazendo as configuracoes padrao do dio

--- lib/_core/services/dio_service.dart
+++ lib/_core/services/dio_service.dart
@@ -5,25 +5,26 @@ import 'package:listin_drift_hive/_core/data/local_data_handler.dart';
 import 'package:listin_drift_hive/listins/data/database.dart';
 
 class DioService {
-  final Dio _dio = Dio();
-
-  static const String url =
-      "https://flutter-dio-c89a9-default-rtdb.firebaseio.com/";
+  final Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: "https://flutter-dio-c89a9-default-rtdb.firebaseio.com/",
+      contentType: 'application/json; charset=utf-8',
+      responseType: ResponseType.json,
+      connectTimeout: const Duration(seconds: 5),
+      receiveTimeout: const Duration(seconds: 3),
+    ),
+  );
 
   Future<void> saveLocalToServer(AppDatabase appDatabase) async {
     Map<String, dynamic> localData = await LocalDataHandler().localDataToMap(
       appdatabase: appDatabase,
     );
 
-    await _dio.put(
-      "${url}listins.json",
-      data: jsonEncode(localData['listins']),
-      options: Options(contentType: 'application/json; charset=utf-8'),
-    );
+    await _dio.put("listins.json", data: jsonEncode(localData['listins']));
   }
 
   getDataFromServer(AppDatabase appDatabase) async {
-    Response response = await _dio.get("${url}listins.json");
+    Response response = await _dio.get("listins.json");
 
     // print(response.statusCode);
     // print(response.headers.toString());
@@ -45,6 +46,6 @@ class DioService {
   }
 
   Future<void> clearServerData() async {
-    await _dio.delete("${url}listins.json");
+    await _dio.delete("listins.json");
   }
 }
