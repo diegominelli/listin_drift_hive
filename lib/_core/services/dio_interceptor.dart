94324002850c3fab0ce664c28a56952abaa75ddc
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2025-09-04 15:30:24 -0300

    usando o logger

--- lib/_core/services/dio_interceptor.dart
+++ lib/_core/services/dio_interceptor.dart
@@ -1,24 +1,36 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
 
 class DioInterceptor extends Interceptor {
+  final Logger _logger = Logger(
+    printer: PrettyPrinter(methodCount: 0, printEmojis: false),
+  );
+
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    // ignore: avoid_print
-    print("${DateTime.now()} | Uma requisição foi feita!");
+    String log = "";
+    log += "REQUISIÇÃO\n";
+    log += "Timestamp: ${DateTime.now()}\n";
+    log += "URL: ${options.uri}\n";
+    log +=
+        "Cabeçalho: ${JsonEncoder.withIndent("  ").convert(options.headers)}\n";
+    if (options.data != null) {
+      log +=
+          "Corpo: ${JsonEncoder.withIndent("  ").convert(json.decode(options.data))}\n";
+    }
+    _logger.w(log);
     super.onRequest(options, handler);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    // ignore: avoid_print
-    print("${DateTime.now()} | Uma resposta foi recebida!");
     super.onResponse(response, handler);
   }
 
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
-    // ignore: avoid_print
-    print("${DateTime.now()} | Um erro aconteceu!");
     super.onError(err, handler);
   }
 }
